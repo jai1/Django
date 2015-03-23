e7b8397765609c114855ac4ae1a9fae5c79242bf
jai1 <jai1@ccis.neu.edu>
2015-03-22 20:00:49 -0700

    Learnt Basic Blog

---
+++ 1-learn/mysite/mysite/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    # blogs.urls Needs to be in quotes - ambiguous error message
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls'))
+)
